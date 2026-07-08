7ecb2191a05dbf289f0771aed55df8c12d179f75
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2026-07-07 22:45:43 -0400

    Add project and reading data

---
+++ src/data/types.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  title: string;
+  description: string;
+  url: string;
+}
+
+export interface ReadingItem {
+  title: string;
+  url: string;
+  source: string;
+  note?: string;
+}
